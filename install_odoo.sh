ce9c6a278cf4e4953f663e8f9c6532eb9447e598
dagisus <jesusreyes.personal@gmail.com>
2018-05-05 22:45:32 -0500

    add iptables reroute and SG port 80 policy

--- install_odoo.sh
+++ install_odoo.sh
@@ -5,3 +5,5 @@ apt-get update && apt-get install odoo -y
 rm /etc/odoo/odoo.conf
 cp /home/ubuntu/odoo.conf /etc/odoo/odoo.conf
 service odoo restart
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8069
+iptables-save
--- vpc_networking.tf
+++ vpc_networking.tf
@@ -10,6 +10,13 @@ resource "aws_security_group" "odooSG_terra" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 22
     to_port     = 22
